b014deb02581da36f2bf6a9d9fc94711f3798bbe
Markus Kahl <machisuji@googlemail.com>
2014-12-16 15:15:39 +0100

    remote file is current file before call to local ~

--- app/uploaders/fog_file_uploader.rb
+++ app/uploaders/fog_file_uploader.rb
@@ -2,14 +2,17 @@ class FogFileUploader < CarrierWave::Uploader::Base
   include FileUploader
 
   storage :fog
-  attr_reader :remote_file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def remote_file
+    @remote_file || file
+  end
+
   def local_file
-    @remote_file = file
+    @remote_file ||= file
     cache_stored_file!
     super
   end
